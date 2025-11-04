d78a348403ce663eff9c56dde85360a7a9fe35b4
Joakim Englund <joakimenglund@protonmail.com>
2025-11-04 11:08:29 +0100

    Created VehicleRepository and refactored Store and BookingRepository since our id's are of different types

--- src/main/java/org/example/dto/VehicleRepository.java
+++ src/main/java/org/example/dto/VehicleRepository.java
@@ -1,6 +1,56 @@
 package org.example.dto;
 
+import org.example.entities.Vehicle;
 import org.example.store.Store;
 
-public class VehicleRepository implements Store {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VehicleRepository implements Store<Vehicle, String> {
+    private final Map<String, Vehicle> store;
+
+    public VehicleRepository() {
+        this.store = new HashMap<>();
+    }
+
+    @Override
+    public List<Vehicle> getAll() {
+        return store.values().stream().toList();
+    }
+
+    @Override
+    public void add(Vehicle obj) {
+        if (obj != null) {
+            store.put(obj.getRegNr(), obj);
+            // Log Vehicle has been added
+        }
+    }
+
+    @Override
+    public Vehicle update(Vehicle obj) {
+        if (obj != null) {
+            Vehicle tmp = store.computeIfPresent(obj.getRegNr(), (k, v) -> obj);
+            if (tmp != null) {
+                // Log Vehicle has been updated
+            }
+            else {
+                // Log error Vehicle not found in system and can therefore not be updated
+            }
+            return tmp;
+        }
+        // Log error Vehicle is null (unless we log this somewhere else)
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+        Vehicle tmp = store.remove(id);
+        if (tmp != null) {
+            // Log Vehicle was removed
+        }
+        else {
+            // Log Vehicle not found in system
+        }
+    }
 }
--- src/main/java/org/example/store/Store.java
+++ src/main/java/org/example/store/Store.java
@@ -2,12 +2,12 @@ package org.example.store;
 
 import java.util.List;
 
-public interface Store<T> {
+public interface Store<T, ID> {
     List<T> getAll();
 
     void add(T obj);
 
     T update(T obj);
 
-    void delete(int id);
+    void delete(ID id);
 }
