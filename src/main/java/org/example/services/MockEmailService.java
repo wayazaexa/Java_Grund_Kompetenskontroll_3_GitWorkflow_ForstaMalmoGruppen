d8002b7bc88292331bc7687eed884631ab3f557b
Joakim Englund <joakimenglund@protonmail.com>
2025-11-06 18:19:54 +0100

    Added initialization and the main app (mocked emailService because I can't focus on setting that up rn)

---
+++ src/main/java/org/example/services/MockEmailService.java
@@ -0,0 +1,16 @@
+package org.example.services;
+
+import org.example.entities.Booking;
+import org.example.entities.BookingStatus;
+import org.example.store.NotificationRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockEmailService implements NotificationRepo {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void notifyBookingEvent(Booking booking, BookingStatus status) {
+        log.info("E-mail has been sent to customer. To: {} E-mail body: The status of your booking is now {}", booking.getEmail(), booking.isReady());
+    }
+}
